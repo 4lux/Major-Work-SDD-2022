c0ad9ec2b47f93b80e91121510c3d88fb7206018
4lux <jonotuite91@gmail.com>
2022-05-11 13:47:56 +1000

    Flashlight Part 3 - On/Off
    Enabled flashlight to be turned on and off

---
+++ Game - SDD MW 2022/Assets/Flashlight_Function.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight_Function : MonoBehaviour
+{
+    public UnityEngine.Rendering.Universal.Light2D fl;
+    private bool status = true;
+    void FixedUpdate()
+    {
+        if (status == true){
+          if (Input.GetMouseButtonDown(0))
+          {
+            fl.enabled = false;
+            status = false;
+          }
+        }
+        else if (status == false) {
+          if (Input.GetMouseButtonDown(0))
+          {
+            fl.enabled = true;
+            status = true;
+          }
+        }
+
+
+    }
+}
