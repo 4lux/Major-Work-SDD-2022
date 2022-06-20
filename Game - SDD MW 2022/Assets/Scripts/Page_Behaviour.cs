b7d1ff8e2b03d684aca40b837353e80ecebc8953
4lux <jonotuite91@gmail.com>
2022-06-21 01:57:15 +1000

    Sorting Layers - Tilesets
    have now enabled the tilesets to have rigid bodies. Started on the item collection

---
+++ Game - SDD MW 2022/Assets/Scripts/Page_Behaviour.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Page_Behaviour : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D Other){
+        Debug.Log("bruh");
+    }
+}
